8b39117f8aba6fdae31b0ff4a48eceb039978199
Adrian <adrianvinicius660@gmail.com>
2022-06-22 21:50:50 -0300

    Mailchimp

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,20 @@ let header = document.getElementsByClassName('header');
     header[0].classList.add('fixed') :
     header[0].classList.remove('fixed')
 } )
+
+$(document).ready(function () {
+    let form = $('form');
+
+    $("form input[type=submit]").bind('click', function(event) {
+        event.preventDefault();
+
+        if (validarCampos(form)) {
+            submeterFormulario(form);
+        }
+        
+    })
+ });
+
+ function validarCampos() {}
+
+ function submeterFormulario() {}
